dd2945dfc7bd19f644236c289c1ded84761a7323
刘学诚 <1350943277liu@gmail.com>
2020-07-15 17:39:11 +0800

    convert data and time

--- plot1.R
+++ plot1.R
@@ -14,7 +14,16 @@ household_power_consumption <- read.table("household_power_consumption.txt",
                                as_tibble() %>%
                                filter(Date == "1/2/2007" | Date == "2/2/2007")
 
+household_power_consumption$Date <- as.Date(household_power_consumption$Date, 
+                                            format = "%d/%m/%Y")
+
+household_power_consumption$Time <- strptime(paste(household_power_consumption$Date, 
+                                                   household_power_consumption$Time, 
+                                                   sep = " "),
+                                             format = "%Y-%m-%d %H:%M:%S")
+
 hist(household_power_consumption$Global_active_power,
      main = "Global Active Power",
      xlab = "Global Active Power(kilowatts)", 
      col = "red")
+
