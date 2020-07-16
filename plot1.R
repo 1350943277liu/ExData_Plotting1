3ea1da57e235cd09ea96a600ff6aff681412f172
刘学诚 <1350943277liu@gmail.com>
2020-07-16 08:58:31 +0800

    complete plot1.r

--- plot1.R
+++ plot1.R
@@ -7,23 +7,16 @@ if (!file.exists("household_power_consumption.txt")) {
         unzip("exdata%2Fdata%2Fhousehold_power_consumption.zip")
 }
 
-household_power_consumption <- read.table("household_power_consumption.txt",
+hpc <- read.table("household_power_consumption.txt",
                                           header = TRUE,
                                           sep = ";",
                                           na.strings = "?") %>%
                                as_tibble() %>%
                                filter(Date == "1/2/2007" | Date == "2/2/2007")
 
-household_power_consumption$Date <- as.Date(household_power_consumption$Date, 
-                                            format = "%d/%m/%Y")
-
-household_power_consumption$Time <- strptime(paste(household_power_consumption$Date, 
-                                                   household_power_consumption$Time, 
-                                                   sep = " "),
-                                             format = "%Y-%m-%d %H:%M:%S")
-
-hist(household_power_consumption$Global_active_power,
+png("plot1.png")
+hist(hpc$Global_active_power,
      main = "Global Active Power",
      xlab = "Global Active Power(kilowatts)", 
      col = "red")
-
+dev.off()
